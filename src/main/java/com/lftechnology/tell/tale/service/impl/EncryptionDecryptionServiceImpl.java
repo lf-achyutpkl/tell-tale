cbd08a589756167e1b85fc2962cecd33b1f2f09a
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-06-24 23:07:06 +0545

    remove key parameter

--- src/main/java/com/lftechnology/tell/tale/service/impl/EncryptionDecryptionServiceImpl.java
+++ src/main/java/com/lftechnology/tell/tale/service/impl/EncryptionDecryptionServiceImpl.java
@@ -1,13 +1,8 @@
 package com.lftechnology.tell.tale.service.impl;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 
 import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESedeKeySpec;
@@ -19,72 +14,76 @@ import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 /**
- * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com>
- * Jun 24, 2016
+ * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com> Jun 24, 2016
  * 
  */
-public class EncryptionDecryptionServiceImpl implements EncryptionDecryptionService{
-	
+public class EncryptionDecryptionServiceImpl implements EncryptionDecryptionService {
+
 	private static final String UNICODE_FORMAT = "UTF8";
-    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
-    private static final String ALGORITHM_KEY = "ThisIsSecretEncryptionKey";
-    private KeySpec keySpec; 
-    private SecretKeyFactory mySecretKeyFactory;
-    private Cipher cipher;
-    byte[] keyAsBytes;
-    SecretKey key;
-    
-    public EncryptionDecryptionServiceImpl() {
-    	try {
+	public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
+	private static final String ALGORITHM_KEY = "ThisIsSecretEncryptionKey";
+	private KeySpec keySpec;
+	private SecretKeyFactory mySecretKeyFactory;
+	private Cipher cipher;
+	byte[] keyAsBytes;
+	SecretKey key;
+
+	public EncryptionDecryptionServiceImpl() {
+		try {
 			cipher = Cipher.getInstance(DESEDE_ENCRYPTION_SCHEME);
 		} catch (Exception e) {
 			throw new EncryptionException();
 		}
-    }
-    
+	}
+
 	@SuppressWarnings("restriction")
 	@Override
-	public String encrypt(String unencryptedString, String key) {
+	public String encrypt(String unencryptedString) {
 		String encryptedString = null;
-        try {
-            cipher.init(Cipher.ENCRYPT_MODE, createKey(key));
-            byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
-            byte[] encryptedText = cipher.doFinal(plainText);
-            BASE64Encoder base64encoder = new BASE64Encoder();
-            encryptedString = base64encoder.encode(encryptedText);
-        } catch (Exception e) {
-           throw new EncryptionException();
-        }
-        return encryptedString;
+		try {
+			cipher.init(Cipher.ENCRYPT_MODE, generateKey());
+			byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
+			byte[] encryptedText = cipher.doFinal(plainText);
+			BASE64Encoder base64encoder = new BASE64Encoder();
+			encryptedString = base64encoder.encode(encryptedText);
+		} catch (Exception e) {
+			throw new EncryptionException();
+		}
+		return encryptedString;
 	}
 
 	@Override
-	public String decrypt(String encryptedString, String key) {
-		String decryptedText=null;
-        try {
-            cipher.init(Cipher.DECRYPT_MODE, createKey(key));
-            BASE64Decoder base64decoder = new BASE64Decoder();
-            byte[] encryptedText = base64decoder.decodeBuffer(encryptedString);
-            byte[] plainText = cipher.doFinal(encryptedText);
-            decryptedText= bytes2String(plainText);
-        } catch (Exception e) {
-        	throw new EncryptionException();
-        }
-        return decryptedText;
+	public String decrypt(String encryptedString) {
+		String decryptedText = null;
+		try {
+			cipher.init(Cipher.DECRYPT_MODE, generateKey());
+			BASE64Decoder base64decoder = new BASE64Decoder();
+			byte[] encryptedText = base64decoder.decodeBuffer(encryptedString);
+			byte[] plainText = cipher.doFinal(encryptedText);
+			decryptedText = bytes2String(plainText);
+		} catch (Exception e) {
+			throw new EncryptionException();
+		}
+		return decryptedText;
 	}
-	
-	private SecretKey createKey(String userKey) throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException{
-		keyAsBytes = userKey.getBytes(UNICODE_FORMAT);
-	    keySpec = new DESedeKeySpec(keyAsBytes);
-	    mySecretKeyFactory = SecretKeyFactory.getInstance(DESEDE_ENCRYPTION_SCHEME);
-	    return mySecretKeyFactory.generateSecret(keySpec);
+
+	private SecretKey generateKey() {
+		try {
+			keyAsBytes = ALGORITHM_KEY.getBytes(UNICODE_FORMAT);
+			keySpec = new DESedeKeySpec(keyAsBytes);
+			mySecretKeyFactory = SecretKeyFactory.getInstance(DESEDE_ENCRYPTION_SCHEME);
+			return mySecretKeyFactory.generateSecret(keySpec);
+		} catch (Exception e) {
+			throw new EncryptionException();
+		}
+
 	}
-	
+
 	private String bytes2String(byte[] bytes) {
-        StringBuffer stringBuffer = new StringBuffer();
-        for (int i = 0; i < bytes.length; i++) {
-            stringBuffer.append((char) bytes[i]);
-        }
-        return stringBuffer.toString();
-    }
+		StringBuffer stringBuffer = new StringBuffer();
+		for (int i = 0; i < bytes.length; i++) {
+			stringBuffer.append((char) bytes[i]);
+		}
+		return stringBuffer.toString();
+	}
 }
